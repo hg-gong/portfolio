30bb0235eb2260d4736f40c436c6cdd2bb09b050
larry <larry@qq.com>
2022-12-07 17:24:42 +0800

    update project

--- src/index.js
+++ src/index.js
@@ -55,9 +55,11 @@ function Projects() {
       <h1>Larry's Projects</h1>
       <p>Here are some of the projects that Larry has worked on:</p>
       <ul>
-        <li><a href="https://github.com/hg-gong/webserver">Webserver</a></li>
-        <li><a href="https://github.com/larry/node-chat-app">Node.js Chat App</a></li>
-        <li><a href="https://github.com/larry/angular-blog">Angular Blog</a></li>
+        <li><a href="https://github.com/hg-gong/webserver">TinyWebserver</a></li>
+        <li><a href="https://github.com/hg-gong/">CMU15445 TinyMySQL</a></li>
+        <li><a href="https://github.com/hg-gong/">CS144 TinyTCP</a></li>
+        <li><a href="https://github.com/hg-gong/">Mit6s081 XV6 Operating System</a></li>
+        <li><a href="https://github.com/hg-gong/">Cpp Trading System</a></li>
       </ul>
     </div>
   );
